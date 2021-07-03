519474b98072256610bae5f11a6c243ac2ef23ea
daniel-beta <danielbetancur@ingenieros.com>
2021-07-03 01:36:08 -0500

    Formulario 2

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+    const [busqueda, guardarBusqueda] = useState({
+        artista: '',
+        cancion: ''
+    })
+    const [ error, guardarError ] = useState(false)
+
+    const {artista, cancion} = busqueda;
+
+    // Función a cada input para leer su contenido
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const buscarInformacion = e => {
+        e.preventDefault();
+
+        if(artista.trim() === '' || cancion.trim() === ''){
+            guardarError(true);
+            return;
+        }
+        guardarError(false)
+        // Todo bien, pasar al componente principal
+    }
+
     return(
-        <h1> Formulario </h1>
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form
+                    onSubmit={buscarInformacion}
+                        className="col card test-white bg-transparent mb-5 pt-5 pb-2"
+                    >
+                        <fieldset>
+                            <legend className="text-center">Buscador de canciones</legend>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Artista</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="artista"
+                                            placeholder="Nombre Artista"
+                                            onChange={actualizarState}
+                                            value={artista}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Canción</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="cancion"
+                                            placeholder="Canción"
+                                            onChange={actualizarState}
+                                            value={cancion}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            >Buscar</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
